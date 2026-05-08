15459f5beb8336728bcc0d2d2d4fc4c531b74769
SDelgado90 <sasha8290@gmail.com>
2026-05-08 19:31:00 -0400

    auth.php

---
+++ includes/auth.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/../config/config.php';
+
+function requireLogin(): void {
+    if (!isset($_SESSION['user'])) {
+        header('Location: login.php');
+        exit;
+    }
+}
+
+function requireRole(array $roles): void {
+    requireLogin();
+
+    if (!in_array($_SESSION['user']['userType'], $roles, true)) {
+        die('Access denied.');
+    }
+}
+?>
